661b576c6a388e8243b6cd6d096fd47371cbe90e
Daniel124 <acha.daniel.874@gmail.com>
2022-10-05 21:13:31 -0400

    Arreglo todo Proyecto

--- application/views/inc/sidebarsbadmin2.php
+++ application/views/inc/sidebarsbadmin2.php
@@ -24,9 +24,6 @@
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Ver:</h6>
-                        <?php echo form_open_multipart('persona/index'); ?>
-                            <button type="submit" class="btn btn-light btn-block btn-sm">Persona</button>
-                        <?php echo form_close(); ?>
                         <?php echo form_open_multipart('profesor/index'); ?>
                             <button type="submit" class="btn btn-light btn-block btn-sm">Profesores</button>
                         <?php echo form_close(); ?>
--- application/views/inscripcion/update.php
+++ application/views/inscripcion/update.php
@@ -9,12 +9,14 @@
         foreach($infoInscritos->result() as $row)
         {
         echo form_open_multipart('inscripcion/modificarbd');?>
+        <input type="hidden" name="idApoderado" value="<?php echo $row->idApoderado; ?>">
+        <input type="hidden" name="idEstudiante" value="<?php echo $row->idEstudiante; ?>">
         <div class="row">
             <div class="col-md-3">
                 <label>Apoderado/a:</label>
             </div>
             <div class="col-md-9">
-                <select class="form-control" required name="idApoderado">
+                <select class="form-control" required name="idApoderado" value="<?php echo $row->idApoderado; ?>">
                     <?php $apoderado=$this->db->query("SELECT idApoderado, CONCAT(persona.nombres, ' ', persona.apellidoPaterno, ' ',persona.apellidoMaterno) AS NombreA
                                                         FROM apoderado
                                                         JOIN persona ON apoderado.idApoderado=persona.idPersona;"); 
@@ -33,7 +35,7 @@
                 <label>Estudiante:</label>
             </div>
             <div class="col-md-9">
-                <select class="form-control" required name="idEstudiante">
+                <select class="form-control" required name="idEstudiante" value="<?php echo $row->idApoderado; ?>">
                     <?php $estudiante=$this->db->query("SELECT idEstudiante, CONCAT(persona.nombres, ' ', persona.apellidoPaterno, ' ',persona.apellidoMaterno) AS NombreE
                                                         FROM estudiante
                                                         JOIN persona ON estudiante.idEstudiante=persona.idPersona;"); 
